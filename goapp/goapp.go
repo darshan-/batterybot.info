cff9ab905125b256cc062ae0253f5e849dd3cde5
Darshan-Josiah Barber <darshan@darshancomputing.com>
2013-07-13 15:01:21 -0700

    Added redirect for /sourcecode

--- goapp/goapp.go
+++ goapp/goapp.go
@@ -6,10 +6,12 @@ import (
 
 const faqURL = "http://code.google.com/p/battery-indicator/wiki/FAQ?tm=6"
 const changelogURL = "http://code.google.com/p/battery-indicator/wiki/Changelog?tm=6"
+const sourceURL = "http://code.google.com/p/battery-indicator/"
 
 func init() {
 	http.HandleFunc("/faq", makeRedirect(faqURL))
 	http.HandleFunc("/changelog", makeRedirect(changelogURL))
+	http.HandleFunc("/sourcecode", makeRedirect(sourceURL))
 }
 
 func makeRedirect(url string) http.HandlerFunc {
